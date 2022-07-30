48a7d31a0fbef6c7c4610cb653369164f9f11f03
Syd <svaughn4@stanford.edu>
2022-07-30 11:52:33 -0700

    Add operator function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,12 @@ function multiply(a, b) {
 function divide(a, b) { 
     return a / b;
 }
+
+// calls the input operation on the 2 numbers
+function operator(string, num1, num2) { 
+    string = string.toLowerCase();
+    if(string === 'add') return add(num1, num2); 
+    else if(string === 'subtract') return subtract(num1, num2); 
+    else if(string === 'multiply') return multiply(num1, num2);
+    else if(string === 'divide') return divide(num1, num2);
+ }
