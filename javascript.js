c99f407e4585303475811ca3133ff816bd036f73
Syd <svaughn4@stanford.edu>
2022-07-31 09:31:05 -0700

    Click button to display digits in screen

--- javascript.js
+++ javascript.js
@@ -14,9 +14,10 @@ function divide(a, b) {
     return a / b;
 }
 
+
 // calls the input operation on the 2 numbers
 function operator(string, num1, num2) { 
-    string = string.toLowerCase();
+    //string = string.toLowerCase();
     if(string === 'add') return add(num1, num2); 
     else if(string === 'subtract') return subtract(num1, num2); 
     else if(string === 'multiply') return multiply(num1, num2);
@@ -24,18 +25,40 @@ function operator(string, num1, num2) {
  }
 
 
+// initiate display variable to show in screen
+let mainNum = 3;
+let display = document.getElementById('display')
+display.innerText= mainNum;
+
+
  // division click event
-document.getElementById('divide').addEventListener('click', operator())
+// document.getElementById('divide').addEventListener('click', operator())
 
-// multiplication click event
-document.getElementById('multiply').addEventListener('click'), operator()
+// // multiplication click event
+// document.getElementById('multiply').addEventListener('click'), operator()
 
-// subtraction click event
-document.getElementById('subtract').addEventListener('click', operator())
+// // subtraction click event
+// document.getElementById('subtract').addEventListener('click', operator())
 
-// addition click event 
-document.getElementById('add').addEventListener('click', operator())
+// // addition click event 
+// document.getElementById('add').addEventListener('click', operator())
 
-// initiate variable to display in calculator screen
-let display;
 
+// add numeric values as click events to digits
+const nums = document.querySelector('.nums')
+const digChildren = nums.childNodes;
+
+// digChildren.forEach(function(item){ 
+//     let x = Number(item.textContent);
+//     console.log(item, x); 
+//     item.addEventListener('click', update(x));
+// });
+
+for(let i = 0; i < 10; i++) { 
+    //console.log((`${i}`));
+    const button = document.getElementById((`${i}`));
+    button.addEventListener('click', () => { 
+        display.innerText = (`${i}`); 
+    });
+    console.log(button);
+}
