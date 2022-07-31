42a3ffef2705fff75ee5a1539af0fc4307c7bafc
Syd <svaughn4@stanford.edu>
2022-07-31 10:52:56 -0700

    Add function sign to calc screen

--- javascript.js
+++ javascript.js
@@ -30,18 +30,37 @@ let display = document.getElementById('display');
 let content = document.createTextNode("");
 display.appendChild(content);
 
+// initiate variables to store the two input nums
+let one;
+let two;
+
 
  // division click event
-// document.getElementById('divide').addEventListener('click', operator())
+document.getElementById('divide').addEventListener('click', () => { 
+    let val = document.getElementById('divide').innerText;
+    content.nodeValue+= val;
+})
 
-// // multiplication click event
-// document.getElementById('multiply').addEventListener('click'), operator()
+// multiplication click event
+document.getElementById('multiply').addEventListener('click', () => {
+    let val = document.getElementById('multiply').innerText;
+    content.nodeValue+= val;
+} )
 
 // // subtraction click event
-// document.getElementById('subtract').addEventListener('click', operator())
+document.getElementById('subtract').addEventListener('click', () => {
+    let val = document.getElementById('subtract').innerText;
+    content.nodeValue+= val;
+})
 
-// // addition click event 
-// document.getElementById('add').addEventListener('click', operator())
+// addition click event 
+document.getElementById('add').addEventListener('click', () => { 
+    let val = document.getElementById('add').innerText;
+    content.nodeValue+= val;
+})
+
+// equal click event aka perform operation
+document.getElementById('equal').addEventListener('click', operator())
 
 // clear click event 
 document.getElementById('clear').addEventListener('click', () => content.nodeValue = "")
@@ -50,5 +69,10 @@ document.getElementById('clear').addEventListener('click', () => content.nodeVal
 for(let i = 0; i < 10; i++) { 
     const button = document.getElementById((`${i}`));
     button.addEventListener('click', () => {
-        content.nodeValue = (`${i}`) });
+        content.nodeValue+= (`${i}`) });
+}
+
+// parse numbers in content field
+function parse() { 
+    
 }
